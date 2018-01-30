911daae0e9fec680670de2f66c057c9b495277c5
Conrad Menz <conrad.menz@dental-wings.com>
2018-01-30 20:55:38 +0100

    * New release and debug script for publishing lambda packages

---
+++ publish-debug.bat
@@ -0,0 +1,5 @@
+del LambdaPackage.zip
+cd "src"
+"C:\Program Files\7-Zip\7z" a -r ..\LambdaPackage.zip * -x!.git
+cd ..
+aws lambda update-function-code --function-name alexa-skill-haltestelle-weststrasse-chemnitz-debug --zip-file fileb://LambdaPackage.zip
